c59bc763b92acba93b76e385479ac7f000c1bcc0
Chetan Gandhi <projectchetan07@gmail.com>
2017-12-02 03:01:16 +0530

    Checkerboard Texture
    - Updated implementation to create texture programmatically.

---
+++ windows/checkerboardTexture/checkerboardTexture.cpp
@@ -0,0 +1,400 @@
+#include <windows.h>
+#include <cstdlib>
+#include <gl/gl.h>
+#include <gl/glu.h>
+
+#include "resources/resource.h"
+
+#define checkImageWidth 64
+#define checkImageHeight 64
+
+HWND hWnd = NULL;
+HDC hdc = NULL;
+HGLRC hrc = NULL;
+
+DWORD dwStyle;
+WINDOWPLACEMENT wpPrev = { sizeof(WINDOWPLACEMENT) };
+RECT windowRect = {0, 0, 800, 600};
+
+bool isFullscreen = false;
+bool isActive = false;
+bool isEscapeKeyPressed = false;
+
+GLubyte checkImage[checkImageHeight][checkImageWidth][4];
+GLuint textureCheckerboard;
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam);
+
+void initialize(void);
+void cleanUp(void);
+void display(void);
+void drawStraightSquare(void);
+void drawTiltedSquare(void);
+void loadGLTexture();
+void makeCheckImage();
+void resize(int width, int height);
+void toggleFullscreen(HWND hWnd, bool isFullscreen);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInsatnce, LPSTR lpszCmdLine, int nCmdShow)
+{
+    WNDCLASSEX wndClassEx;
+    MSG message;
+    TCHAR szApplicationTitle[] = TEXT("CG - Checkerboard Texture");
+    TCHAR szApplicationClassName[] = TEXT("RTR_CHECKERBOARD_TEXTURE");
+    bool done = false;
+
+    wndClassEx.cbSize = sizeof(WNDCLASSEX);
+    wndClassEx.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+    wndClassEx.cbClsExtra = 0;
+    wndClassEx.cbWndExtra = 0;
+    wndClassEx.lpfnWndProc = WndProc;
+    wndClassEx.hInstance = hInstance;
+    wndClassEx.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(CP_ICON));
+    wndClassEx.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(CP_ICON_SMALL));
+    wndClassEx.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wndClassEx.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+    wndClassEx.lpszClassName = szApplicationClassName;
+    wndClassEx.lpszMenuName = NULL;
+
+    if(!RegisterClassEx(&wndClassEx))
+    {
+        MessageBox(NULL, TEXT("Cannot register class."), TEXT("Error"), MB_OK | MB_ICONERROR);
+        exit(EXIT_FAILURE);
+    }
+
+    DWORD styleExtra = WS_EX_APPWINDOW;
+    dwStyle = WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN | WS_CLIPSIBLINGS | WS_VISIBLE;
+
+    hWnd = CreateWindowEx(styleExtra,
+        szApplicationClassName,
+        szApplicationTitle,
+        dwStyle,
+        windowRect.left,
+        windowRect.top,
+        windowRect.right - windowRect.left,
+        windowRect.bottom - windowRect.top,
+        NULL,
+        NULL,
+        hInstance,
+        NULL);
+
+    if(!hWnd)
+    {
+        MessageBox(NULL, TEXT("Cannot create windows."), TEXT("Error"), MB_OK | MB_ICONERROR);
+        exit(EXIT_FAILURE);
+    }
+
+    initialize();
+
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+    SetForegroundWindow(hWnd);
+    SetFocus(hWnd);
+
+    while(!done)
+    {
+        if(PeekMessage(&message, NULL, 0, 0, PM_REMOVE))
+        {
+            if(message.message == WM_QUIT)
+            {
+                done = true;
+            }
+            else
+            {
+                TranslateMessage(&message);
+                DispatchMessage(&message);
+            }
+        }
+        else
+        {
+            if(isActive)
+            {
+                if(isEscapeKeyPressed)
+                {
+                    done = true;
+                }
+                else
+                {
+                    display();
+                }
+            }
+        }
+    }
+
+    cleanUp();
+
+    return (int)message.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
+{
+    HDC hdc;
+    RECT rect;
+
+    switch(iMessage)
+    {
+        case WM_ACTIVATE:
+            isActive = (HIWORD(wParam) == 0);
+        break;
+
+        case WM_SIZE:
+            resize(LOWORD(lParam), HIWORD(lParam));
+        break;
+
+        case WM_KEYDOWN:
+            switch(wParam)
+            {
+                case VK_ESCAPE:
+                    isEscapeKeyPressed = true;
+                break;
+
+                // 0x46 is hex value for key 'F' or 'f'
+                case 0x46:
+                    isFullscreen = !isFullscreen;
+                    toggleFullscreen(hWnd, isFullscreen);
+                break;
+
+                default:
+                break;
+            }
+
+        break;
+
+        case WM_CHAR:
+            switch(wParam)
+            {
+            }
+        break;
+
+        case WM_LBUTTONDOWN:
+        break;
+
+        case WM_DESTROY:
+            PostQuitMessage(0);
+        break;
+
+        default:
+        break;
+    }
+
+    return DefWindowProc(hWnd, iMessage, wParam, lParam);
+}
+
+void initialize(void)
+{
+    PIXELFORMATDESCRIPTOR pfd;
+    int pixelFormatIndex = 0;
+
+    ZeroMemory(&pfd, sizeof(PIXELFORMATDESCRIPTOR));
+
+    pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
+    pfd.nVersion = 1;
+    pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+    pfd.iPixelType = PFD_TYPE_RGBA;
+    pfd.cColorBits = 32;
+    pfd.cRedBits = 8;
+    pfd.cGreenBits = 8;
+    pfd.cBlueBits = 8;
+    pfd.cAlphaBits = 8;
+    pfd.cDepthBits = 32;
+
+    hdc = GetDC(hWnd);
+
+    pixelFormatIndex = ChoosePixelFormat(hdc, &pfd);
+
+    if(pixelFormatIndex == 0)
+    {
+        ReleaseDC(hWnd, hdc);
+        hdc = NULL;
+    }
+
+    if(!SetPixelFormat(hdc, pixelFormatIndex, &pfd))
+    {
+        ReleaseDC(hWnd, hdc);
+        hdc = NULL;
+    }
+
+    hrc = wglCreateContext(hdc);
+    if(hrc == NULL)
+    {
+        ReleaseDC(hWnd, hdc);
+        hdc = NULL;
+    }
+
+    if(!wglMakeCurrent(hdc, hrc))
+    {
+        wglDeleteContext(hrc);
+        hrc = NULL;
+
+        ReleaseDC(hWnd, hdc);
+        hdc = NULL;
+    }
+
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClearDepth(1.0f);
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+    glShadeModel(GL_SMOOTH);
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+
+    glEnable(GL_TEXTURE_2D);
+    loadGLTexture();
+
+    // This is required for DirectX
+    resize(windowRect.right - windowRect.left, windowRect.bottom - windowRect.top);
+}
+
+void display(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glTranslatef(0.0f, 0.0f, -3.6f);
+
+    drawStraightSquare();
+    drawTiltedSquare();
+
+    SwapBuffers(hdc);
+}
+
+void drawStraightSquare(void)
+{
+    glBegin(GL_QUADS);
+
+	glTexCoord3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(-2.0f, -1.0f, 0.0f);
+
+    glTexCoord3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(-2.0f, 1.0f, 0.0f);
+
+    glTexCoord3f(1.0f, 1.0f, 0.0f);
+	glVertex3f(0.0f, 1.0f, 0.0f);
+
+    glTexCoord3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(0.0f, -1.0f, 0.0f);
+
+    glEnd();
+}
+
+void drawTiltedSquare(void)
+{
+    glBegin(GL_QUADS);
+
+    glTexCoord3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(1.0f, -1.0f, 0.0f);
+
+	glTexCoord3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(1.0f, 1.0f, 0.0f);
+
+    glTexCoord3f(1.0f, 1.0f, 0.0f);
+	glVertex3f(2.41421f, 1.0f, -1.41421f);
+
+    glTexCoord3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(2.41421f, -1.0f, -1.41421f);
+
+    glEnd();
+}
+
+void loadGLTexture()
+{
+    makeCheckImage();
+    glGenTextures(1, &textureCheckerboard);
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
+    glBindTexture(GL_TEXTURE_2D, textureCheckerboard);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, checkImageWidth, checkImageHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, checkImage);
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+}
+
+void makeCheckImage(void)
+{
+	int heightCounter = 0;
+    int widthCounter = 0;
+    int colorWhite = 0;
+
+	for (heightCounter = 0; heightCounter < checkImageHeight; ++heightCounter)
+	{
+		for (widthCounter = 0; widthCounter < checkImageWidth; ++widthCounter)
+		{
+			colorWhite = (((heightCounter & 0x8) == 0) ^ ((widthCounter & 0x8) == 0)) * 255;
+			checkImage[heightCounter][widthCounter][0] = (GLubyte)colorWhite;
+			checkImage[heightCounter][widthCounter][1] = (GLubyte)colorWhite;
+			checkImage[heightCounter][widthCounter][2] = (GLubyte)colorWhite;
+			checkImage[heightCounter][widthCounter][3] = (GLubyte)255;
+		}
+	}
+}
+
+void resize(int width, int height)
+{
+    if(height == 0)
+    {
+        height = 1;
+    }
+
+    glViewport(0, 0, (GLsizei)width, (GLsizei)height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(45.0f, (GLfloat)width/(GLfloat)height, -1.0f, 1.0f);
+}
+
+void toggleFullscreen(HWND hWnd, bool isFullscreen)
+{
+    MONITORINFO monitorInfo;
+    dwStyle = GetWindowLong(hWnd, GWL_STYLE);
+
+    if(isFullscreen)
+    {
+        if(dwStyle & WS_OVERLAPPEDWINDOW)
+        {
+            monitorInfo = { sizeof(MONITORINFO) };
+
+            if(GetWindowPlacement(hWnd, &wpPrev) && GetMonitorInfo(MonitorFromWindow(hWnd, MONITORINFOF_PRIMARY), &monitorInfo))
+            {
+                SetWindowLong(hWnd, GWL_STYLE, dwStyle & ~WS_OVERLAPPEDWINDOW);
+                SetWindowPos(hWnd, HWND_TOP, monitorInfo.rcMonitor.left, monitorInfo.rcMonitor.top, monitorInfo.rcMonitor.right - monitorInfo.rcMonitor.left, monitorInfo.rcMonitor.bottom - monitorInfo.rcMonitor.top, SWP_NOZORDER | SWP_FRAMECHANGED);
+            }
+        }
+
+        ShowCursor(FALSE);
+    }
+    else
+    {
+        SetWindowLong(hWnd, GWL_STYLE, dwStyle | WS_OVERLAPPEDWINDOW);
+        SetWindowPlacement(hWnd, &wpPrev);
+        SetWindowPos(hWnd, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOOWNERZORDER | SWP_NOZORDER | SWP_FRAMECHANGED);
+        ShowCursor(TRUE);
+    }
+}
+
+void cleanUp(void)
+{
+    if (textureCheckerboard)
+	{
+		glDeleteTextures(1, &textureCheckerboard);
+		textureCheckerboard = 0;
+	}
+
+    if(isFullscreen)
+    {
+        dwStyle = GetWindowLong(hWnd, GWL_STYLE);
+        SetWindowLong(hWnd, GWL_STYLE, dwStyle | WS_OVERLAPPEDWINDOW);
+        SetWindowPlacement(hWnd, &wpPrev);
+        SetWindowPos(hWnd, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOOWNERZORDER | SWP_NOZORDER | SWP_FRAMECHANGED);
+        ShowCursor(TRUE);
+    }
+
+    wglMakeCurrent(NULL, NULL);
+
+    wglDeleteContext(hrc);
+    hrc = NULL;
+
+    ReleaseDC(hWnd, hdc);
+    hdc = NULL;
+
+    DestroyWindow(hWnd);
+    hWnd = NULL;
+}
