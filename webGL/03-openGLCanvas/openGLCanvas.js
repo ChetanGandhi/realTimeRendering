c1c06ed345e07114c85cf9cdeede74d61b8aeeb2
Chetan Gandhi <projectchetan07@gmail.com>
2018-04-08 15:46:41 +0530

    WebGL:  OpenGL Canvas
    - Updated implementation to draw blue colored OpenGL canvas.

---
+++ webGL/03-openGLCanvas/openGLCanvas.js
@@ -0,0 +1,129 @@
+let surface = null;
+let gl = null;
+let isFullscreen = false;
+let surfaceOriginalWidth = 1;
+let surfaceOriginalHeight = 1;
+let requestAnimationFrame =
+    window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame;
+
+let cancelAnimationFrame =
+    window.cancelAnimationFrame ||
+    window.webkitCancelRequestAnimationFrame ||
+    window.webkitCancelAnimationFrame ||
+    window.mozCancelRequestAnimationFrame ||
+    window.mozCancelAnimationFrame ||
+    window.oCancelRequestAnimationFrame ||
+    window.oCancelAnimationFrame ||
+    window.msCancelRequestAnimationFrame ||
+    window.msCancelAnimationFrame;
+
+function main() {
+    surface = document.getElementById("surface");
+
+    if (!surface) {
+        console.log("CG | Error | Not able to get surface.");
+        return;
+    }
+
+    console.log("CG | Info | Surface found.");
+    console.log("CG | Info | Surface width:", surface.width, "surface height:", surface.height);
+
+    surfaceOriginalWidth = surface.width;
+    surfaceOriginalHeight = surface.height;
+
+    window.addEventListener("keydown", onKeyDown, false);
+    window.addEventListener("click", onMouseDown, false);
+    window.addEventListener("resize", onResize, false);
+
+    initialize();
+}
+
+function onKeyDown(event) {
+    switch (event.key) {
+        case "F":
+        case "f":
+            toggleFullscreen();
+            break;
+    }
+}
+
+function onMouseDown(event) {}
+
+function onResize(event) {
+    if (isFullscreen) {
+        resize(window.innerWidth, window.innerHeight);
+    } else {
+        resize(surfaceOriginalWidth, surfaceOriginalHeight);
+    }
+}
+
+function initialize() {
+    gl = surface.getContext("webgl2");
+
+    if (!gl) {
+        console.log("CG | Error | Not able to get WebGL-2 context.");
+        return;
+    }
+
+    console.log("CG | Info | 2D gl found.");
+
+    gl.viewportWidth = surface.width;
+    gl.viewportHeight = surface.height;
+
+    gl.clearColor(0.0, 0.0, 1.0, 1.0);
+
+    resize(surfaceOriginalWidth, surfaceOriginalHeight);
+    display();
+}
+
+function display() {
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    requestAnimationFrame(display, surface);
+}
+
+function toggleFullscreen() {
+    let fullscreenElement =
+        document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement || document.msFullscreenElement || null;
+
+    if (!fullscreenElement) {
+        if (surface.requestFullscreen) {
+            surface.requestFullscreen();
+        } else if (surface.mozRequestFullScreen) {
+            surface.mozRequestFullScreen();
+        } else if (surface.webkitRequestFullscreen) {
+            surface.webkitRequestFullscreen();
+        } else if (surface.msRequestFullscreen) {
+            surface.msRequestFullscreen();
+        }
+
+        isFullscreen = true;
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        }
+
+        isFullscreen = false;
+    }
+}
+
+function resize(width, height) {
+    if (height === 0) {
+        height = 1;
+    }
+
+    surface.width = width;
+    surface.height = height;
+
+    gl.viewport(0, 0, width, height);
+}
---
+++ webGL/03-openGLCanvas/readme.md
@@ -0,0 +1,8 @@
+# OpenGL Canvas
+
+###### Preview
+
+![openGLCanvas][opengl-canvas-image]
+
+[//]: # "Image declaration"
+[opengl-canvas-image]: ./preview/openGLCanvas.png " OpenGL Canvas"
