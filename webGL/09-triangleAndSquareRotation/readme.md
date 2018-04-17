f8aa198b5b4733d54a05a3559ba90a7ea26433f5
Chetan Gandhi <projectchetan07@gmail.com>
2018-04-18 04:07:42 +0530

    WebGL: Triangle and Square Rotation
    - Updated implementation to rotate triangle and square.
    - Enabled depth test.

---
+++ webGL/09-triangleAndSquareRotation/readme.md
@@ -0,0 +1,14 @@
+# Triangle and Square Rotation
+
+###### Keyboard shortcuts
+
+*   Press `Esc` key to quit.
+*   Press `f` key to toggle fullscreen mode.
+*   Press `1` to `9` key to increase the rotation speed.
+
+###### Preview
+
+![triangleAndSquareRotation][triangle-and-square-rotation-image]
+
+[//]: # "Image declaration"
+[triangle-and-square-rotation-image]: ./preview/triangleAndSquareRotation.png "Triangle and Square Rotation"
---
+++ webGL/09-triangleAndSquareRotation/triangleAndSquareRotation.js
@@ -0,0 +1,468 @@
+let surface = null;
+let gl = null;
+let isEscapeKeyPressed = false;
+let isFullscreen = false;
+let surfaceOriginalWidth = 1;
+let surfaceOriginalHeight = 1;
+let displayAnimationRequestId = 0;
+let requestAnimationFrame =
+    window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame;
+
+let cancelAnimationFrame =
+    window.cancelAnimationFrame ||
+    window.webkitCancelRequestAnimationFrame ||
+    window.webkitCancelAnimationFrame ||
+    window.mozCancelRequestAnimationFrame ||
+    window.mozCancelAnimationFrame ||
+    window.oCancelRequestAnimationFrame ||
+    window.oCancelAnimationFrame ||
+    window.msCancelRequestAnimationFrame ||
+    window.msCancelAnimationFrame;
+
+const WebGLMacros = {
+    CG_ATTRIBUTE_VERTEX: 0,
+    CG_ATTRIBUTE_COLOR: 1,
+    CG_ATTRIBUTE_NORMAL: 2,
+    CG_ATTRIBUTE_TEXTURE0: 3
+};
+
+let angleTriangle = 0.0;
+let angleSquare = 0.0;
+let speed = 0.3;
+
+let vertexShaderObject = null;
+let fragmentShaderObject = null;
+let shaderProgramObject = null;
+
+let vaoTriangle = null;
+let vboTrianglePosition = null;
+let vboTriangleColor = null;
+let vaoSquare = null;
+let vboSquarePosition = null;
+let vboSquareColor = null;
+let mvpUniform = null;
+
+let perspectiveProjectionMatrix = null;
+
+function main() {
+    surface = document.getElementById("surface");
+
+    if (!surface) {
+        console.log("CG | Error | Not able to get surface.");
+        return;
+    }
+
+    console.log("CG | Info | Surface found.");
+    console.log("CG | Info | Surface width:", surface.width, "surface height:", surface.height);
+
+    surfaceOriginalWidth = surface.width;
+    surfaceOriginalHeight = surface.height;
+
+    window.addEventListener("keydown", onKeyDown, false);
+    window.addEventListener("click", onMouseDown, false);
+    window.addEventListener("resize", onResize, false);
+
+    initialize();
+    display();
+}
+
+function onKeyDown(event) {
+    if (!event.repeat) {
+        return;
+    }
+
+    switch (event.key) {
+        case "F":
+        case "f":
+            toggleFullscreen();
+            break;
+
+        case "Escape":
+            if (!isEscapeKeyPressed) {
+                isEscapeKeyPressed = true;
+                cleanUp();
+                alert("Rendering is stopped.");
+            }
+            break;
+
+        case "1":
+            speed = 0.1;
+            break;
+
+        case "2":
+            speed = 0.1 * 2;
+            break;
+
+        case "3":
+            speed = 0.1 * 3;
+            break;
+
+        case "4":
+            speed = 0.1 * 4;
+            break;
+
+        case "5":
+            speed = 0.1 * 5;
+            break;
+
+        case "6":
+            speed = 0.1 * 6;
+            break;
+
+        case "7":
+            speed = 0.1 * 7;
+            break;
+
+        case "8":
+            speed = 0.1 * 8;
+            break;
+
+        case "9":
+            speed = 0.1 * 9;
+            break;
+
+        default:
+            console.log("Keydown not handled:", event.key);
+            break;
+    }
+}
+
+function onMouseDown(event) {}
+
+function onResize(event) {
+    if (isFullscreen) {
+        resize(window.innerWidth, window.innerHeight);
+    } else {
+        resize(surfaceOriginalWidth, surfaceOriginalHeight);
+    }
+}
+
+function initialize() {
+    gl = surface.getContext("webgl2");
+
+    if (!gl) {
+        console.log("CG | Error | Not able to get WebGL-2 context.");
+        return;
+    }
+
+    console.log("CG | Info | WebGL2 context found.");
+
+    gl.viewportWidth = surface.width;
+    gl.viewportHeight = surface.height;
+
+    initializeVertexShaderObject();
+    initializeFragmentShaderObject();
+    initializeShaderProgramObject();
+    initializeTriangleBuffers();
+    initializeSquareBuffers();
+
+    perspectiveProjectionMatrix = mat4.create();
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clearDepth(1.0);
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+
+    resize(surfaceOriginalWidth, surfaceOriginalHeight);
+}
+
+function initializeVertexShaderObject() {
+    let vertexShaderCode =
+        "#version 300 es" +
+        "\n" +
+        "in vec4 vertexPosition;" +
+        "in vec4 vertexColor;" +
+        "\n" +
+        "out vec4 outVertexColor;" +
+        "\n" +
+        "uniform mat4 mvpMatrix;" +
+        "\n" +
+        "void main(void)" +
+        "{" +
+        "   gl_Position = mvpMatrix * vertexPosition;" +
+        "   outVertexColor = vertexColor;" +
+        "}";
+
+    vertexShaderObject = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShaderObject, vertexShaderCode);
+    gl.compileShader(vertexShaderObject);
+
+    if (!gl.getShaderParameter(vertexShaderObject, gl.COMPILE_STATUS)) {
+        let error = gl.getShaderInfoLog(vertexShaderObject);
+
+        if (error.length > 0) {
+            alert(error);
+            cleanUp();
+        }
+    }
+}
+
+function initializeFragmentShaderObject() {
+    let fragmentShaderCode =
+        "#version 300 es" +
+        "\n" +
+        "precision highp float;" +
+        "\n" +
+        "in vec4 outVertexColor;" +
+        "\n" +
+        "out vec4 fragmentColor;" +
+        "\n" +
+        "void main(void)" +
+        "{" +
+        "   fragmentColor = outVertexColor;" +
+        "}";
+
+    fragmentShaderObject = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragmentShaderObject, fragmentShaderCode);
+    gl.compileShader(fragmentShaderObject);
+
+    if (!gl.getShaderParameter(fragmentShaderObject, gl.COMPILE_STATUS)) {
+        let error = gl.getShaderInfoLog(fragmentShaderObject);
+
+        if (error.length > 0) {
+            alert(error);
+            cleanUp();
+        }
+    }
+}
+
+function initializeShaderProgramObject() {
+    shaderProgramObject = gl.createProgram();
+    gl.attachShader(shaderProgramObject, vertexShaderObject);
+    gl.attachShader(shaderProgramObject, fragmentShaderObject);
+    gl.bindAttribLocation(shaderProgramObject, WebGLMacros.CG_ATTRIBUTE_VERTEX, "vertexPosition");
+    gl.bindAttribLocation(shaderProgramObject, WebGLMacros.CG_ATTRIBUTE_COLOR, "vertexColor");
+    gl.linkProgram(shaderProgramObject);
+
+    if (!gl.getProgramParameter(shaderProgramObject, gl.LINK_STATUS)) {
+        let error = gl.getProgramInfoLog(shaderProgramObject);
+
+        if (error.length > 0) {
+            alert(error);
+            cleanUp();
+        }
+    }
+
+    mvpUniform = gl.getUniformLocation(shaderProgramObject, "mvpMatrix");
+}
+
+function initializeTriangleBuffers() {
+    let triangleVertices = new Float32Array([0.0, 1.0, 0.0, -1.0, -1.0, 0.0, 1.0, -1.0, 0.0]);
+    let triangleColors = new Float32Array([1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0]);
+
+    vaoTriangle = gl.createVertexArray();
+    gl.bindVertexArray(vaoTriangle);
+
+    vboTrianglePosition = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vboTrianglePosition);
+    gl.bufferData(gl.ARRAY_BUFFER, triangleVertices, gl.STATIC_DRAW);
+    gl.vertexAttribPointer(WebGLMacros.CG_ATTRIBUTE_VERTEX, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(WebGLMacros.CG_ATTRIBUTE_VERTEX);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+    vboTriangleColor = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vboTriangleColor);
+    gl.bufferData(gl.ARRAY_BUFFER, triangleColors, gl.STATIC_DRAW);
+    gl.vertexAttribPointer(WebGLMacros.CG_ATTRIBUTE_COLOR, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(WebGLMacros.CG_ATTRIBUTE_COLOR);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+    gl.bindVertexArray(null);
+}
+
+function initializeSquareBuffers() {
+    let squareVertices = new Float32Array([-1.0, 1.0, 0.0, -1.0, -1.0, 0.0, 1.0, -1.0, 0.0, 1.0, 1.0, 0.0]);
+
+    vaoSquare = gl.createVertexArray();
+    gl.bindVertexArray(vaoSquare);
+
+    vboSquarePosition = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vboSquarePosition);
+    gl.bufferData(gl.ARRAY_BUFFER, squareVertices, gl.STATIC_DRAW);
+    gl.vertexAttribPointer(WebGLMacros.CG_ATTRIBUTE_VERTEX, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(WebGLMacros.CG_ATTRIBUTE_VERTEX);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+    gl.vertexAttrib3f(WebGLMacros.CG_ATTRIBUTE_COLOR, 0.392156, 0.584313, 0.929411);
+
+    gl.bindVertexArray(null);
+}
+
+function update() {
+    angleSquare -= speed;
+    angleTriangle += speed;
+
+    if (angleSquare <= -360.0) {
+        angleSquare = 0.0;
+    }
+
+    if (angleTriangle >= 360.0) {
+        angleTriangle = 0.0;
+    }
+}
+
+function display() {
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.useProgram(shaderProgramObject);
+
+    drawTriangle();
+    drawSquare();
+
+    gl.useProgram(null);
+
+    update();
+    displayAnimationRequestId = requestAnimationFrame(display);
+}
+
+function drawTriangle() {
+    let modelViewMatrix = mat4.create();
+    let rotationMatrix = mat4.create();
+    let modelViewProjectionMatrix = mat4.create();
+
+    mat4.translate(modelViewMatrix, modelViewMatrix, [-1.5, 0.0, -6.0]);
+    mat4.rotateY(rotationMatrix, rotationMatrix, degreeToRadian(angleTriangle));
+
+    mat4.multiply(modelViewMatrix, modelViewMatrix, rotationMatrix);
+    mat4.multiply(modelViewProjectionMatrix, perspectiveProjectionMatrix, modelViewMatrix);
+    gl.uniformMatrix4fv(mvpUniform, false, modelViewProjectionMatrix);
+
+    gl.bindVertexArray(vaoTriangle);
+
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
+
+    gl.bindVertexArray(null);
+}
+
+function drawSquare() {
+    let modelViewMatrix = mat4.create();
+    let rotationMatrix = mat4.create();
+    let modelViewProjectionMatrix = mat4.create();
+
+    mat4.translate(modelViewMatrix, modelViewMatrix, [1.5, 0.0, -6.0]);
+    mat4.rotateX(rotationMatrix, rotationMatrix, degreeToRadian(angleSquare));
+
+    mat4.multiply(modelViewMatrix, modelViewMatrix, rotationMatrix);
+    mat4.multiply(modelViewProjectionMatrix, perspectiveProjectionMatrix, modelViewMatrix);
+    gl.uniformMatrix4fv(mvpUniform, false, modelViewProjectionMatrix);
+
+    gl.bindVertexArray(vaoSquare);
+
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
+
+    gl.bindVertexArray(null);
+}
+
+function degreeToRadian(degree) {
+    return degree * Math.PI / 180.0;
+}
+
+function toggleFullscreen() {
+    let fullscreenElement =
+        document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement || document.msFullscreenElement || null;
+
+    if (!fullscreenElement) {
+        if (surface.requestFullscreen) {
+            surface.requestFullscreen();
+        } else if (surface.mozRequestFullScreen) {
+            surface.mozRequestFullScreen();
+        } else if (surface.webkitRequestFullscreen) {
+            surface.webkitRequestFullscreen();
+        } else if (surface.msRequestFullscreen) {
+            surface.msRequestFullscreen();
+        }
+
+        isFullscreen = true;
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        }
+
+        isFullscreen = false;
+    }
+}
+
+function resize(width, height) {
+    if (height == 0) {
+        height = 1;
+    }
+
+    surface.width = width;
+    surface.height = height;
+
+    gl.viewport(0, 0, width, height);
+    mat4.perspective(perspectiveProjectionMatrix, 45.0, width / height, 1.0, 100.0);
+}
+
+function cleanUp() {
+    cancelAnimationFrame(displayAnimationRequestId);
+
+    if (vaoTriangle) {
+        gl.deleteVertexArray(vaoTriangle);
+        vaoTriangle = null;
+    }
+
+    if (vboTrianglePosition) {
+        gl.deleteBuffer(vboTrianglePosition);
+        vboTrianglePosition = null;
+    }
+
+    if (vboTriangleColor) {
+        gl.deleteBuffer(vboTriangleColor);
+        vboTriangleColor = null;
+    }
+
+    if (vaoSquare) {
+        gl.deleteVertexArray(vaoSquare);
+        vaoSquare = null;
+    }
+
+    if (vboSquarePosition) {
+        gl.deleteBuffer(vboSquarePosition);
+        vboSquarePosition = null;
+    }
+
+    if (vboSquareColor) {
+        gl.deleteBuffer(vboSquareColor);
+        vboSquareColor = null;
+    }
+
+    if (shaderProgramObject) {
+        if (vertexShaderObject) {
+            gl.detachShader(shaderProgramObject, vertexShaderObject);
+        }
+
+        if (fragmentShaderObject) {
+            gl.detachShader(shaderProgramObject, fragmentShaderObject);
+        }
+    }
+
+    if (vertexShaderObject) {
+        gl.deleteShader(vertexShaderObject);
+        vertexShaderObject = null;
+    }
+
+    if (fragmentShaderObject) {
+        gl.deleteShader(fragmentShaderObject);
+        fragmentShaderObject = null;
+    }
+
+    if (shaderProgramObject) {
+        gl.deleteProgram(shaderProgramObject);
+        shaderProgramObject = null;
+    }
+
+    gl.useProgram(null);
+}
