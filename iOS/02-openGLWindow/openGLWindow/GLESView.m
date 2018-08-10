be88b571d0a4e38d928a5faaceb05f6633f40b68
Chetan Gandhi <projectchetan07@gmail.com>
2018-08-11 00:36:23 +0530

    iOS: OpenGL Window
    - Updated implementation to draw openGL window.

---
+++ iOS/02-openGLWindow/openGLWindow/GLESView.m
@@ -0,0 +1,240 @@
+#import "GLESView.h"
+
+#import <OpenGLES/ES3/gl.h>
+#import <OpenGLES/ES3/glext.h>
+
+@implementation GLESView
+{
+    EAGLContext *context;
+
+    GLuint defaultFramebuffer;
+    GLuint colorRenderbuffer;
+    GLuint depthRenderbuffer;
+
+    CADisplayLink *displayLink;
+
+    NSInteger aimationFrameInterval;
+
+    BOOL isRendering;
+}
+
+-(instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+
+    if(self)
+    {
+        NSMutableDictionary *drawableProperties = [[NSMutableDictionary alloc] init];
+        [drawableProperties setObject:[NSNumber numberWithBool:NO] forKey:kEAGLDrawablePropertyRetainedBacking];
+        [drawableProperties setObject:kEAGLColorFormatRGBA8 forKey:kEAGLDrawablePropertyColorFormat];
+
+        CAEAGLLayer *layer = (CAEAGLLayer *)super.layer;
+        layer.opaque = NO;
+        layer.drawableProperties = drawableProperties;
+
+        context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES3];
+        
+        [drawableProperties release];
+        drawableProperties = nil;
+
+        if(context == nil)
+        {
+            NSLog(@"[Error] | Cannot create OpenGL ES 3.0 context");
+            [self release];
+            return nil;
+        }
+
+        [EAGLContext setCurrentContext:context];
+
+        glGenFramebuffers(1, &defaultFramebuffer);
+        glGenRenderbuffers(1, &colorRenderbuffer);
+        glBindFramebuffer(GL_FRAMEBUFFER, defaultFramebuffer);
+        glBindRenderbuffer(GL_RENDERBUFFER, colorRenderbuffer);
+
+        [context renderbufferStorage:GL_RENDERBUFFER fromDrawable:layer];
+
+        glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, colorRenderbuffer);
+
+        GLint backingWidth = 0;
+        GLint backingHeight = 0;
+
+        glGetRenderbufferParameteriv(GL_RENDERBUFFER, GL_RENDERBUFFER_WIDTH, &backingWidth);
+        glGetRenderbufferParameteriv(GL_RENDERBUFFER, GL_RENDERBUFFER_HEIGHT, &backingHeight);
+
+        glGenRenderbuffers(1, &depthRenderbuffer);
+        glBindRenderbuffer(GL_RENDERBUFFER, depthRenderbuffer);
+        glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT16, backingWidth, backingHeight);
+        glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthRenderbuffer);
+
+        if(glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+        {
+            NSLog(@"[Error] | Cannot create complete framebuffer object, status: %x", glCheckFramebufferStatus(GL_FRAMEBUFFER));
+            glDeleteFramebuffers(1, &defaultFramebuffer);
+            glDeleteRenderbuffers(1, &colorRenderbuffer);
+            glDeleteRenderbuffers(1, &depthRenderbuffer);
+
+            return nil;
+        }
+
+        NSLog(@"[Info] | Renderer: %s", glGetString(GL_RENDERER));
+        NSLog(@"[Info] | OpenGL ES version: %s", glGetString(GL_VERSION));
+        NSLog(@"[Info] | GLSL version: %s", glGetString(GL_SHADING_LANGUAGE_VERSION));
+
+        isRendering = NO;
+        aimationFrameInterval = 60;
+
+        glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+
+        UITapGestureRecognizer *doubleTapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(onDoubleTap:)];
+        [doubleTapGestureRecognizer setNumberOfTapsRequired:2];
+        [doubleTapGestureRecognizer setNumberOfTouchesRequired:1];
+        [doubleTapGestureRecognizer setDelegate:self];
+        [self addGestureRecognizer:doubleTapGestureRecognizer];
+
+        UITapGestureRecognizer *singleTapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(onSingleTap:)];
+        [singleTapGestureRecognizer setNumberOfTapsRequired:1];
+        [singleTapGestureRecognizer setNumberOfTouchesRequired:1];
+        [singleTapGestureRecognizer setDelegate:self];
+        [singleTapGestureRecognizer requireGestureRecognizerToFail:doubleTapGestureRecognizer];
+        [self addGestureRecognizer:singleTapGestureRecognizer];
+
+        UILongPressGestureRecognizer *longPressGestureRecognizer = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(onLongPress:)];
+        [self addGestureRecognizer:longPressGestureRecognizer];
+
+        UISwipeGestureRecognizer *swipeGestureRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(onSwip:)];
+        [self addGestureRecognizer:swipeGestureRecognizer];
+    }
+
+    return self;
+}
+
+-(BOOL)acceptsFirstResponder
+{
+    return YES;
+}
+
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+//    [self setNeedsDisplay];
+}
+
+-(void)onSingleTap:(UIGestureRecognizer *)gestureRecognizer
+{
+    [self setNeedsDisplay];
+}
+
+-(void)onDoubleTap:(UIGestureRecognizer *)gestureRecognizer
+{
+    [self setNeedsDisplay];
+}
+
+-(void)onLongPress:(UILongPressGestureRecognizer *)gestureRecognizer
+{
+    [self setNeedsDisplay];
+}
+
+-(void)onSwip:(UISwipeGestureRecognizer *)gestureRecognizer
+{
+    [self release];
+    exit(EXIT_SUCCESS);
+}
+
++(Class)layerClass
+{
+    return [CAEAGLLayer class];
+}
+
+-(void)render:(id)sender
+{
+    [EAGLContext setCurrentContext:context];
+
+    glBindFramebuffer(GL_FRAMEBUFFER, defaultFramebuffer);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+    glBindRenderbuffer(GL_RENDERBUFFER, colorRenderbuffer);
+
+    [context presentRenderbuffer:GL_RENDERBUFFER];
+}
+
+-(void)layoutSubviews
+{
+    GLint width = 0;
+    GLint height = 0;
+
+    glBindRenderbuffer(GL_RENDERBUFFER, colorRenderbuffer);
+    [context renderbufferStorage:GL_RENDERBUFFER fromDrawable:(CAEAGLLayer *)self.layer];
+
+    glGetRenderbufferParameteriv(GL_RENDERBUFFER, GL_RENDERBUFFER_WIDTH, &width);
+    glGetRenderbufferParameteriv(GL_RENDERBUFFER, GL_RENDERBUFFER_HEIGHT, &height);
+
+    glGenRenderbuffers(1, &depthRenderbuffer);
+    glBindRenderbuffer(GL_RENDERBUFFER, depthRenderbuffer);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT16, width, height);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthRenderbuffer);
+
+    glViewport(0, 0, width, height);
+
+    if(glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+    {
+        NSLog(@"[Error] | Cannot create complete framebuffer object, status: %x", glCheckFramebufferStatus(GL_FRAMEBUFFER));
+    }
+
+    [self render: nil];
+}
+
+-(void)startRendering
+{
+    if(!isRendering)
+    {
+        Class displayLinkClass = NSClassFromString(@"CADisplayLink");
+
+        displayLink = [displayLinkClass displayLinkWithTarget:self selector:@selector(render:)];
+        [displayLink setPreferredFramesPerSecond:aimationFrameInterval];
+        [displayLink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+
+        isRendering = YES;
+    }
+}
+
+-(void)stopRendering
+{
+    if(isRendering)
+    {
+        [displayLink invalidate];
+        displayLink = nil;
+
+        isRendering = NO;
+    }
+}
+
+-(void)dealloc
+{
+    if(depthRenderbuffer)
+    {
+        glDeleteRenderbuffers(1, &depthRenderbuffer);
+        depthRenderbuffer = 0;
+    }
+
+    if(colorRenderbuffer)
+    {
+        glDeleteRenderbuffers(1, &colorRenderbuffer);
+        colorRenderbuffer = 0;
+    }
+
+    if(defaultFramebuffer)
+    {
+        glDeleteFramebuffers(1, &defaultFramebuffer);
+        defaultFramebuffer = 0;
+    }
+
+    if([EAGLContext currentContext] == context)
+    {
+        [EAGLContext setCurrentContext:nil];
+    }
+
+    [context release];
+    context = nil;
+
+    [super dealloc];
+}
+
+@end
