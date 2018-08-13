dbd857ea9115610c5bf064dc3aa4e999d1711d3c
Chetan Gandhi <projectchetan07@gmail.com>
2018-08-14 01:50:36 +0530

    OpenAL
    - Updated implementation to switch different sounds.
    - Changed implementation to use 'p' to play, pause and resume.
    - Updated implementation to cleanup audio buffers.
    - Deleted unwanted audio files.
    - Added new audio files.

--- openAL/main.cpp
+++ openAL/main.cpp
@@ -9,13 +9,17 @@ int main(int argc, char const *argv[])
     bool done = false;
     ALfloat soundSourcePosition[3] = {0.0f, 0.0f, 0.0f};
     ALuint audioBufferId = 0;
-	AudioManager *audioManage = NULL;
+    ALuint defaultAudioBufferId_1 = 0;
+    ALuint defaultAudioBufferId_2 = 0;
+    ALuint defaultAudioBufferId_3 = 0;
+    ALuint currentAudioBufferId = 0;
+    AudioManager *audioManage = NULL;
     SoundSource *soundSource = NULL;
 
     if(argc != 2)
     {
+        printf("\n\tYou can also pass wav file as command line arguments to play it.\n");
         printf("\n\tUsage: %s waveFilePath.wav\n", argv[0]);
-        exit(EXIT_FAILURE);
     }
 
     audioManage = new AudioManager();
@@ -23,27 +27,46 @@ int main(int argc, char const *argv[])
     audioManage->setListenerPosition(0.0f, 0.0f, 0.0f);
     audioManage->setListenerVelocity(0.0f, 0.0f, 0.0f);
 
-    alGenBuffers(1, &audioBufferId);
+    if(argc == 2)
+    {
+        alGenBuffers(1, &audioBufferId);
+        ALboolean userAudioLoaded = audioManage->loadWaveAudio(argv[1], audioBufferId);
 
-    ALboolean waveDataLoaded = audioManage->loadWaveAudio(argv[1], audioBufferId);
+        if(userAudioLoaded)
+        {
+            currentAudioBufferId = audioBufferId;
+        }
+        else
+        {
+            printf("\nNot able to open wave file '%s'\n", argv[1]);
+            alDeleteBuffers(1, &audioBufferId);
+            audioBufferId = 0;
+        }
+    }
+
+    alGenBuffers(1, &defaultAudioBufferId_1);
+    alGenBuffers(1, &defaultAudioBufferId_2);
+    alGenBuffers(1, &defaultAudioBufferId_3);
+
+    ALboolean waveDataLoaded = audioManage->loadWaveAudio("resources/audio/sound1.wav", defaultAudioBufferId_1);
+    waveDataLoaded = audioManage->loadWaveAudio("resources/audio/sound2.wav", defaultAudioBufferId_2);
+    waveDataLoaded = audioManage->loadWaveAudio("resources/audio/sound3.wav", defaultAudioBufferId_3);
 
     if(waveDataLoaded)
     {
         printf("\nq: quit\n");
-        printf("p: pause\n");
-        printf("r: resume\n");
-        printf("s: start or stop playing\n");
+        printf("p: play, pause, or resume\n");
+        printf("s: stop\n");
         printf("l: toggle looping\n");
         printf("x: set source x position\n");
         printf("y: set source y position\n");
         printf("z: set source z position\n");
+        printf("n: switch to next audio\n");
 
         soundSource = new SoundSource();
         soundSource->setSourcef(AL_ROLLOFF_FACTOR, 1.0f);
         soundSource->setSourcef(AL_REFERENCE_DISTANCE, 1.0f);
-        soundSource->play(audioBufferId);
 
-        printf("\nplaying...\n");
         printf(":");
 
         char c = ':';
@@ -62,16 +85,26 @@ int main(int argc, char const *argv[])
 
                     done = true;
                     printf("quit\n");
+
                 break;
 
                 case 'p':
-                    soundSource->pause();
-                    printf("paused\n");
-                break;
+                    if(soundSource->isPlaying())
+                    {
+                        soundSource->pause();
+                        printf("paused\n");
+                    }
+                    else if(soundSource->isPaused())
+                    {
+                        soundSource->resume();
+                        printf("resuming\n");
+                    }
+                    else
+                    {
+                        soundSource->play(currentAudioBufferId);
+                        printf("playing\n");
+                    }
 
-                case 'r':
-                    soundSource->resume();
-                    printf("resuming\n");
                 break;
 
                 case 's':
@@ -80,11 +113,7 @@ int main(int argc, char const *argv[])
                         soundSource->stop();
                         printf("stoped\n");
                     }
-                    else
-                    {
-                        soundSource->play(audioBufferId);
-                        printf("playing...\n");
-                    }
+
                 break;
 
                 case 'l':
@@ -122,6 +151,39 @@ int main(int argc, char const *argv[])
 
                 break;
 
+                case 'n':
+                    soundSource->stop();
+
+                    if(currentAudioBufferId == audioBufferId)
+                    {
+                        currentAudioBufferId = defaultAudioBufferId_1;
+                        printf("playing default 1\n");
+                    }
+                    else if(currentAudioBufferId == defaultAudioBufferId_1)
+                    {
+                        currentAudioBufferId = defaultAudioBufferId_2;
+                        printf("playing default 2\n");
+                    }
+                    else if(currentAudioBufferId == defaultAudioBufferId_2)
+                    {
+                        currentAudioBufferId = defaultAudioBufferId_3;
+                        printf("playing default 3\n");
+                    }
+                    else if(currentAudioBufferId == defaultAudioBufferId_3 && audioBufferId != 0)
+                    {
+                        currentAudioBufferId = audioBufferId;
+                        printf("playing user audio\n");
+                    }
+                    else
+                    {
+                        currentAudioBufferId = defaultAudioBufferId_1;
+                        printf("playing default 1\n");
+                    }
+
+                    soundSource->play(currentAudioBufferId);
+
+                break;
+
                 default:
                 break;
             }
@@ -129,17 +191,50 @@ int main(int argc, char const *argv[])
             fflush(stdin);
 	    } while (!done);
     }
+    else
+    {
+        printf("\nUnable to open default sounds.\n");
+    }
 
-    printf("\n");
+    printf("\nCleaning up...\n");
 
-    delete soundSource;
-    soundSource = NULL;
+    currentAudioBufferId = 0;
 
-    alDeleteBuffers(1, &audioBufferId);
-    audioBufferId = 0;
+    if(soundSource != NULL)
+    {
+        delete soundSource;
+        soundSource = NULL;
+    }
 
-    delete audioManage;
-    audioManage = NULL;
+    if(audioBufferId != 0)
+    {
+        alDeleteBuffers(1, &audioBufferId);
+        audioBufferId = 0;
+    }
+
+    if(defaultAudioBufferId_1 != 0)
+    {
+        alDeleteBuffers(1, &defaultAudioBufferId_1);
+        defaultAudioBufferId_1 = 0;
+    }
+
+    if(defaultAudioBufferId_2 != 0)
+    {
+        alDeleteBuffers(1, &defaultAudioBufferId_2);
+        defaultAudioBufferId_2 = 0;
+    }
+
+    if(defaultAudioBufferId_3 != 0)
+    {
+        alDeleteBuffers(1, &defaultAudioBufferId_3);
+        defaultAudioBufferId_3 = 0;
+    }
+
+    if(audioManage != NULL)
+    {
+        delete audioManage;
+        audioManage = NULL;
+    }
 
     return 0;
 }
