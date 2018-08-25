dbba2ca45fa455cba2891dd5c99f57a66ccfc08b
Chetan Gandhi <ChetanGandhi@users.noreply.github.com>
2018-08-25 15:27:56 +0530

    maxOS: Build All
    - Added script to build all assignments.

---
+++ macOS/buildAll.sh
@@ -0,0 +1,45 @@
+macOSDir=$(pwd)
+echo Current path: $macOSDir
+
+cd 01-window && chmod +x ./build.sh && ./build.sh && echo 01-window...done
+cd $macOSDir
+cd 02-openGLWindow && chmod +x ./build.sh && ./build.sh && echo 02-openGLWindow...done
+cd $macOSDir
+cd 03-triangleOrtho && chmod +x ./build.sh && ./build.sh && echo 03-triangleOrtho...done
+cd $macOSDir
+cd 04-trianglePerspective && chmod +x ./build.sh && ./build.sh && echo 04-trianglePerspective...done
+cd $macOSDir
+cd 05-multiColoredTriangle && chmod +x ./build.sh && ./build.sh && echo 05-multiColoredTriangle...done
+cd $macOSDir
+cd 06-triangleAndSquare && chmod +x ./build.sh && ./build.sh && echo 06-triangleAndSquare...done
+cd $macOSDir
+cd 07-triangleAndSquareColored && chmod +x ./build.sh && ./build.sh && echo 07-triangleAndSquareColored...done
+cd $macOSDir
+cd 08-triangleAndSquareRotation && chmod +x ./build.sh && ./build.sh && echo 08-triangleAndSquareRotation...done
+cd $macOSDir
+cd 09-pyramidAndCubeRotation && chmod +x ./build.sh && ./build.sh && echo 09-pyramidAndCubeRotation...done
+cd $macOSDir
+cd 10-pyramidAndCubeTexture && chmod +x ./build.sh && ./build.sh && echo 10-pyramidAndCubeTexture...done
+cd $macOSDir
+cd 11-smiley && chmod +x ./build.sh && ./build.sh && echo 11-smiley...done
+cd $macOSDir
+cd 12-smileyTweaked && chmod +x ./build.sh && ./build.sh && echo 12-smileyTweaked...done
+cd $macOSDir
+cd 13-checkerboardTexture && chmod +x ./build.sh && ./build.sh && echo 13-checkerboardTexture...done
+cd $macOSDir
+cd 14-rotatingCubeWithLight && chmod +x ./build.sh && ./build.sh && echo 14-rotatingCubeWithLight...done
+cd $macOSDir
+cd 15-sphereWithLight && chmod +x ./build.sh && ./build.sh && echo 15-sphereWithLight...done
+cd $macOSDir
+cd 16-perVertexLight && chmod +x ./build.sh && ./build.sh && echo 16-perVertexLight...done
+cd $macOSDir
+cd 17-perFragmentLight && chmod +x ./build.sh && ./build.sh && echo 17-perFragmentLight...done
+cd $macOSDir
+cd 18-perVertexAndFragmentLight && chmod +x ./build.sh && ./build.sh && echo 18-perVertexAndFragmentLight...done
+cd $macOSDir
+cd 19-pyramidMultipleLight && chmod +x ./build.sh && ./build.sh && echo 19-pyramidMultipleLight...done
+cd $macOSDir
+cd 20-multipleLightsOnSphere && chmod +x ./build.sh && ./build.sh && echo 20-multipleLightsOnSphere...done
+cd $macOSDir
+cd 21-materialSphere && chmod +x ./build.sh && ./build.sh && echo 21-materialSphere...done
+cd $macOSDir
