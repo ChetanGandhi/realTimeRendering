04cbb5b308277549215e97e39785ea3179199c30
Chetan Gandhi <projectchetan07@gmail.com>
2018-09-06 01:26:52 +0530

    DirectX: Smiley
    - Updated implementation to draw smiley testure.

---
+++ directX/11-smiley/build.bat
@@ -0,0 +1,6 @@
+set root=%CD%
+cd resources
+rc.exe /V resource.rc
+cd %root%
+
+cl.exe /EHsc /DUNICODE /Zi smiley.cpp /link /NODEFAULTLIB:msvcrt.lib resources\resource.res  /LIBPATH:".\lib\directXTK" user32.lib kernel32.lib gdi32.lib d3d11.lib D3dcompiler.lib DirectXTK.lib
